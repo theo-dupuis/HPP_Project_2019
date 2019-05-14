5e0eb8508a40866013137cafbef93285b2f531df
theo-dupuis <tseinfo@info346-vm>
2019-05-14 14:57:54 +0200

    Add comment constructor

--- src/main/java/p2019/Comment.java
+++ src/main/java/p2019/Comment.java
@@ -1,5 +1,30 @@
 package p2019;
 
-public class Comment {
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Comment {
+	
+	private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS+0000");
+	private Date timeStamp_;
+	private int commentId_;
+	private String comment_;
+	
+	public Comment(String ts, String commentId, String comment)
+	{
+		try {
+			timeStamp_ = format.parse(ts);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		commentId_ = Integer.parseInt(commentId);
+		comment_ = comment;
+	}
+	
+	
+	
 }
