d26b9d05ba6b32b076ac26b088ee882fcb37316e
theo-dupuis <tseinfo@info346-vm>
2019-05-14 15:33:17 +0200

    Add Like constructor

--- src/main/java/p2019/Like.java
+++ src/main/java/p2019/Like.java
@@ -1,9 +1,26 @@
 package p2019;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Like {
 
-	public Like(String string, String string2, String string3) {
-		// TODO Auto-generated constructor stub
+	private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS+0000");
+	private Date timeStamp_;
+	private int commentId_;
+	private int userId_;
+	
+	public Like(String ts, String commentId, String userId) {
+		try {
+			timeStamp_ = format.parse(ts);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		commentId_ = Integer.parseInt(commentId);
+		userId_ = Integer.parseInt(userId);
 	}
 
 }
