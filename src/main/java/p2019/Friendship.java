38267855514b57eb7d1a166dd8c6f9441f57e181
theo-dupuis <tseinfo@info346-vm>
2019-05-14 15:29:18 +0200

    Add Friendship constructor

--- src/main/java/p2019/Friendship.java
+++ src/main/java/p2019/Friendship.java
@@ -1,9 +1,26 @@
 package p2019;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Friendship {
 
-	public Friendship(String string, String string2, String string3) {
-		// TODO Auto-generated constructor stub
+
+	private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS+0000");
+	private Date timeStamp_;
+	private int user1Id_;
+	private int user2Id_;
+	public Friendship(String ts, String user1Id, String user2Id) {
+		try {
+			timeStamp_ = format.parse(ts);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		user1Id_ = Integer.parseInt(user1Id);
+		user2Id_ = Integer.parseInt(user2Id);
 	}
 
 }
