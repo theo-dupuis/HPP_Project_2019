102a737ea2d40b3520e47eda5108aa21c4a9131e
theo-dupuis <theodupuis08@gmail.com>
2019-05-15 16:03:07 +0200

    add timeStamps processing

--- src/main/java/p2019/MyApp.java
+++ src/main/java/p2019/MyApp.java
@@ -9,10 +9,12 @@ public class MyApp {
 	public static Map<String, Comment> comments = new HashMap<>();
 	public static Map<String, User> users = new HashMap<>();
 	public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS+0000");
-	
+	public static int duration;
+	public static int k;
 	
 	public static void main(String[] args) {
-		
+		duration = Integer.parseInt(args[0]);
+		k = Integer.parseInt(args[1]);
 	}
 
 }
