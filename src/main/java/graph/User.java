101dccacbca91e532860dffab38d5c9052d6f8e1
Romain Coudour <romain.coudour@telecom-st-etienne.fr>
2019-05-15 14:13:04 +0200

    User : POJO

--- src/main/java/graph/User.java
+++ src/main/java/graph/User.java
@@ -1,48 +1,53 @@
 package graph;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Observable;
 import java.util.Set;
 
 import p2019.Comment;
-import p2019.Like;
-import p2019.MyApp;
 
-public class User extends Observable {
+public class User {
 	
 	private String id;
 	private Set<User> friends;
+	private Set<Comment> comments;
 	
 	public User(String id) {
 		this.id = id;
 		friends  = new HashSet<>();
+		comments  = new HashSet<>();
+
 	}
 
-	public void addFriend(User friend, String timestamp)
-	{
-		friends.add(friend);
-		setChanged();
-		notifyObservers(timestamp);
-	}
-	
-	public void addLike(Like like)
-	{
-		Comment comment = MyApp.comments.get(like.getComment());
-		addObserver(comment);
-		comment.processLike(like.getTimeStamp(), this);
-		
-	}
-	
-	public String getId()
-	{
+	public String getId() {
 		return id;
 	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Set<User> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(Set<User> friends) {
+		this.friends = friends;
+	}
+
+	public Set<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(Set<Comment> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((comments == null) ? 0 : comments.hashCode());
+		result = prime * result + ((friends == null) ? 0 : friends.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
@@ -56,6 +61,16 @@ public class User extends Observable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
+		if (comments == null) {
+			if (other.comments != null)
+				return false;
+		} else if (!comments.equals(other.comments))
+			return false;
+		if (friends == null) {
+			if (other.friends != null)
+				return false;
+		} else if (!friends.equals(other.friends))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -63,8 +78,6 @@ public class User extends Observable {
 			return false;
 		return true;
 	}
+
 	
-	public boolean isFriend(User usr) {
-		return friends.contains(usr);
-	}
 }
