9c473230337316262a6872de816d709839a9d498
Romain Coudour <romain.coudour@telecom-st-etienne.fr>
2019-05-15 14:20:41 +0200

    Comment : Refactoring

--- src/main/java/p2019/Comment.java
+++ src/main/java/p2019/Comment.java
@@ -11,87 +11,29 @@ import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
-import graph.User;
-
-public class Comment implements Observer{
+public class Comment {
 	
-	private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS+0000");
-	private Date creationTimeStamp_;
-	private Date lastUpdateTimeStamp_;
-	private String commentId_;
-	private String comment_;
-	private int score_;
-	private List<User> likers;
+	private String id;
+	
+	private Date creationTimeStamp;
+	private Date lastUpdateTimeStamp;
+	
+	private int range;
+	private String content;
+	private List<Community> communities;
 	
 	public Comment(String ts, String commentId, String comment)
 	{
 		try {
-			creationTimeStamp_ = format.parse(ts);
-			lastUpdateTimeStamp_ = creationTimeStamp_;
+			creationTimeStamp = MyApp.dateFormat.parse(ts);
+			lastUpdateTimeStamp = creationTimeStamp;
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Error while formatting timestamp");
 		}
 		
-		commentId_ = commentId;
-		comment_ = comment;
-		score_ =0;
-		likers = new ArrayList<>();
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + commentId_;
-		return result;
-	}
-
-	public void addLiker(User liker)
-	{
-		likers.put(liker.getId(), liker);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Comment other = (Comment) obj;
-		if (commentId_ != other.commentId_)
-			return false;
-		return true;
-	}
-
-	@Override
-	public void update(Observable o, Object arg) {
-		try {
-			lastUpdateTimeStamp_ = format.parse((String)arg);
-		} catch (ParseException e) {
-			System.out.println("Error while updating timestamp");
-		}
-		
-		processFriendship();
-		
-	}
-	
-	public void processLike(String ts, User user) {
-		try {
-			lastUpdateTimeStamp_ = format.parse(ts);
-		} catch (ParseException e) {
-			System.out.println("Error while updating timestamp");
-		}
-		
-		for(User u : likers)
-			if(user.isFriend(u))
-				score_+=1; // Score chez tous les utilisateurs ?
-		
-	}
-	
-	public void processFriendship() {
-		
+		id = commentId;
+		content = comment;
+		range =0;
+		communities = new ArrayList<>();
 	}
 }
--- src/main/java/p2019/MyApp.java
+++ src/main/java/p2019/MyApp.java
@@ -1,5 +1,6 @@
 package p2019;
 
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,6 +12,9 @@ public class MyApp {
 	
 	public static Graph friendshipGraph = new Graph();
 	public static Map<String, Comment> comments = new HashMap<>();
+	public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS+0000");
+	
+	
 	public static void main(String[] args) {
 		
 	}
