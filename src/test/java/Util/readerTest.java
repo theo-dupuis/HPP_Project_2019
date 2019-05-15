7d156cdc3517b7dcb649d2892218ea20a6de2cbc
Thomas <urufu.mion@gmail.com>
2019-05-15 14:54:38 +0200

    update reader test & del unnecessary test

--- src/test/java/Util/readerTest.java
+++ src/test/java/Util/readerTest.java
@@ -14,13 +14,9 @@ public class readerTest {
 		URL url = Thread.currentThread().getContextClassLoader().getResource("Test_Cases/T1_comments.dat");
 		String fileToReadPath = url.getPath();
 		String firstLineStringReaded = "2010-12-22T02:14:32.827+0000|34359738627|99|I see|Baoping Wu||529360";
-		// TODO ReadFirstLine
-		String firstLineReaded = "toto";//TODO
-		//assertEquals(firstLineStringReaded, firstLineReaded);
-		Reader commentReader = new Reader("comment");
-		File file = new File(fileToReadPath);
-		assertTrue(file.exists());
-		commentReader.processFile(fileToReadPath);
+		Reader reader = new Reader("comment");
+		reader.processFile(fileToReadPath);
+		// TODO verification of a added queue object
 	}
 
 }
