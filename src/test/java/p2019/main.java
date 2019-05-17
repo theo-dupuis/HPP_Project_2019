5ebbf7306d90bfcd9554b152ac5ec08ef48a769f
Thomas <urufu.mion@gmail.com>
2019-05-17 12:21:53 +0200

    Update main.java

--- src/test/java/p2019/main.java
+++ src/test/java/p2019/main.java
@@ -1,14 +1,9 @@
 package p2019;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import org.junit.Before;
 import org.junit.Test;
 
